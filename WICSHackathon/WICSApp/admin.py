73cbbe350ca50e169400402670fdfd207a65034c
Adam <adampruner99@gmail.com>
2016-11-19 14:59:50 -0800

    added dynamic view functionality, following tutorial 3

--- WICSHackathon/WICSApp/admin.py
+++ WICSHackathon/WICSApp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Quote
+
+admin.site.register(Quote)
--- WICSHackathon/WICSApp/models.py
+++ WICSHackathon/WICSApp/models.py
@@ -1,7 +1,18 @@
 from __future__ import unicode_literals
 
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import python_2_unicode_compatible
+
+@python_2_unicode_compatible # needed to support Python 2
 class Quote(models.Model):
     quote_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.quote_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
