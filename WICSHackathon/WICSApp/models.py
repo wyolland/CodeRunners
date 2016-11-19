008d4bfba054539d350322f722c6ebaf43a78a1f
Adam <adampruner99@gmail.com>
2016-11-19 11:51:55 -0800

    made some migrations and migrated them, created quote model

--- WICSHackathon/WICSApp/models.py
+++ WICSHackathon/WICSApp/models.py
@@ -2,4 +2,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Quote(models.Model):
+    quote_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
