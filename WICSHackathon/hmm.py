a4f6625948eec89a3f32869c1fe0574763aa0486
Adam <adampruner99@gmail.com>
2016-11-19 19:53:32 -0800

    added grammar checking and correction, requires lib3to2 and language-check (can be installed via pip)

--- WICSHackathon/hmm.py
+++ WICSHackathon/hmm.py
@@ -2,12 +2,13 @@
 import csv
 import random
 import nltk
+import language_check
 from nltk import tokenize
 from nltk.tag import hmm
 
-QUOTES_TO_GEN = 100
-UPPER_QUOTE = 20
-LOWER_QUOTE = 7
+tool = language_check.LanguageTool('en-US')
+
+QUOTES_TO_GEN = 10
 
 rng = random.random()
 
@@ -33,11 +34,12 @@ modelHMM = trainer.train(train_data)
 f = open("input.txt", 'w')
 
 for i in range(QUOTES_TO_GEN):
-    quote_list = modelHMM.random_sample(random,random.randrange(LOWER_QUOTE, UPPER_QUOTE))
-    LENGTH = len(quote_list)
+    quote_list = modelHMM.random_sample(random,random.randrange(7, 20))
     quote_str = ""
-    for j in range(0, LENGTH):
-        quote_str = (quote_str + quote_list[j][0] + " ") if j+1 < LENGTH else (quote_str + quote_list[j][0] + ".\n")
+    for j in range(0, len(quote_list)):
+        quote_str = (quote_str + quote_list[j][0] + " ") if j+1 < len(quote_list) else (quote_str + quote_list[j][0] + ".\n")
+    matches = tool.check(quote_str)
+    quote_str = language_check.correct(quote_str, matches)
     f.write(quote_str)
 
 f.close()
