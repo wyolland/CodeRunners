9bd4143a304e6fc4cd18d1f0cf2e3ab8b06d0d5e
Adam <adampruner99@gmail.com>
2016-11-19 20:56:12 -0800

    added random button stuff

--- WICSHackathon/WICSApp/views.py
+++ WICSHackathon/WICSApp/views.py
@@ -2,6 +2,7 @@
 # from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, render
+from random import randint
 
 from .models import Quote
 
@@ -27,7 +28,7 @@ def downvote(request, quote_id):
     return render(request, 'WICSApp/index.html', context)
 
 def random(request, quote_id):
-    quote = get_object_or_404(Quote, pk=quote_id)
+    quote = get_object_or_404(Quote, pk=randint(1,Quote.objects.count()+1))
     context = {'quote': quote}
     return render(request, 'WICSApp/random.html', context)
 
@@ -39,9 +40,10 @@ def detail(request, quote_id):
 def results(request, quote_id):
     response = "You're looking at the results of quote %s."
     return HttpResponse(response % quote_id)
-
+'''
 def random(request):
     return Quote.objects.filter(id=randint(1,Quote.objects.count()+1))
+'''
 
 # remove this view later
 def vote(request, quote_id):
